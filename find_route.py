1131b966e57b9d529512ef5f2a61c1928f7e324d
Milo Dietrick <milodietrick@gmail.com>
2019-01-18 11:23:59 -0700

    graph has been created, needs verifying

--- find_route.py
+++ find_route.py
@@ -6,16 +6,37 @@
 
 #libraries
 from pathlib import Path
+
+
 input_file = str( Path.cwd() / 'input1.txt')
 with open (input_file, "r") as myfile:
-     data=myfile.readlines()
-print(data)
+     data=myfile.read().splitlines()
+
+#Now i need to create a graph
+# start = []
+# end = []
+# dist = []
+
+# for string in data:
+
 
 #def find_route (input_file, origin_city, dest_city):
-    #read the input file, the file contains two cities per line and the distance 
-    #between them
-
 
     #thoughts: use a dictionary to create a graph of nodes and
+#i need to create a map (dictionary) that will hold each of the cities and the cities
+#connected to it as well as the distances between them
+#so maybe a dictionary within a dictionary? now how do i creat that
+graph = dict()
+for i in data:
+    if i == "END OF INPUT": break    
+    node = {i.split()[1]: i.split()[2]}
+    start = i.split()[0]
+    if start in graph: #if the start city is in the set then it has been added
+        #add the corresponding node and the distance
+        graph[start].append(node)
+    else: # this is a new node add it to the dictionary
+        graph[start] = [node]
 
+for i in graph:
+    print (i, graph[i])
 
